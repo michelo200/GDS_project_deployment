c49518d97f022ac0b1773454b58314c6a819f92d
micp <micp@implement.dk>
2024-04-13 09:58:57 +0200

    added app folder

---
+++ app/tools/ui_utils.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+
+def ui_setup():
+    # Removing 'Deploy' button from the top right corner for cooler demo experience
+    st.markdown("""
+        <style>
+            .reportview-container {
+                margin-top: -2em;
+            }
+            #MainMenu {visibility: hidden;}
+            .stDeployButton {display:none;}
+            footer {visibility: hidden;}
+            #stDecoration {display:none;}
+        </style>
+    """, unsafe_allow_html=True)
+
+def add_logo():
+    image_path = "visuals/itu-logo.png"
+    left, middle, right = st.columns([10,1,5])
+    with right:
+        st.image(image_path, use_column_width=True)
---
+++ app/tools/utils.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import time
+import numpy as np
+
+from tools.ui_utils import (
+    add_logo,
+    ui_setup
+)
