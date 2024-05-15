d13e97f335d75699b479afd2bbc435a98ffbfa7d
micp <micp@implement.dk>
2024-05-15 11:24:23 +0200

    commenting

--- app/tools/utils.py
+++ app/tools/utils.py
@@ -12,19 +12,26 @@ import plotly.express as px
 def get_amenity_data():
     pois = gpd.read_file("../dataframes/clean_pois_montreal.geojson")
     pois['geometry'] = pois['geometry'].centroid
+    
     polygons = gpd.read_file("../dataframes/district_polygons.geojson")
     polygons['centroids'] = polygons['geometry'].centroid
     centroids = gpd.GeoDataFrame(polygons, geometry='centroids')
+    
     centroids = centroids.drop(columns='geometry').rename(columns={'centroids': 'geometry'})
     centroids = gpd.GeoDataFrame(centroids, geometry='geometry')
+    
     amenities_with_neighborhood = gpd.sjoin(pois, polygons, how="left", op="within")
     amenities_with_neighborhood['distance_to_centroid'] = amenities_with_neighborhood.geometry.distance(polygons.geometry)
     # amenities_with_neighborhood.centroid
     # amenities_with_neighborhood.geometry
+    
     polygons_centroids = amenities_with_neighborhood[['Arrondissement','centroids']].copy()
+    
     amenities_with_neighborhood.drop(columns=['centroids'], inplace=True)
+    
     amenities_with_neighborhood['distance_to_centroid'] = amenities_with_neighborhood.geometry.distance(polygons_centroids.centroids)
     amenities_with_neighborhood['distance_in_m'] = amenities_with_neighborhood['distance_to_centroid']*111195
+    
     return amenities_with_neighborhood, polygons
 
 
@@ -37,19 +44,12 @@ def amenity_dataframe():
     df = pd.DataFrame(amenities_with_neighborhood)
     df.rename(columns={"Arrondissement": "neighbourhood", "distance_in_m": "distance_in_m_from_center"}, inplace=True)
     
-    amenities = df['amenity'].unique().tolist()
-    categories = df['category'].unique().tolist()
-    neighbourhoods = df['neighbourhood'].unique().tolist()
-    
-    # use streamlit columns to include all selectboxes in one row
     st.write("Here you have to possibility to browse through all amenity and neighbourhood data below.")
     st.write("**Filter by category, amenity, and neighbourhood, if you like.**")
 
-
-    # Create columns for the filter type, specific filter, and reset button
+    # columns for the filter type, specific filter, and reset button
     col1, col2 = st.columns([1, 3])
 
-    # Create a radio button for selecting the filter type
     with col1:
         filter_type = st.radio("Filter by:", ("Amenity", "Category"), key="filter_type")
         
@@ -64,7 +64,6 @@ def amenity_dataframe():
         if neighbourhood_selectbox:
             filtered_df = filtered_df[filtered_df['neighbourhood'] == neighbourhood_selectbox]
 
-    # Create a selectbox for choosing the specific amenity or category
     with col2:
         st.dataframe(data=filtered_df, hide_index=True, use_container_width=True)
 
@@ -81,14 +80,11 @@ def amenity_plot():
     df = pd.DataFrame(amenities_with_neighborhood)
     df.rename(columns={"Arrondissement": "neighbourhood", "distance_in_m": "distance_in_m_from_center"}, inplace=True)
     
-    # Create columns for the filter type and specific filter
     col1, col2 = st.columns(2)
 
-    # Create a radio button for selecting the filter type
     with col1:
         filter_type = st.radio("Filter by:", ("Amenity", "Category"))
 
-    # Create a selectbox for choosing the specific amenity or category
     with col2:
         if filter_type == "Amenity":
             selected_filter = st.selectbox("Select Amenity:", [''] + df['amenity'].unique().tolist())
@@ -97,15 +93,15 @@ def amenity_plot():
             selected_filter = st.selectbox("Select Category:", [''] + df['category'].unique().tolist())
             filtered_df = df[df['category'] == selected_filter] if selected_filter else df
 
-    # Count the occurrences based on the selected filter and neighborhood
+    # occurrences based on the selected filter and neighborhood
     count_df = filtered_df.groupby(['neighbourhood', filter_type.lower()]).size().reset_index(name='count')    
     
-    # Create the Plotly bar chart
+    # plotting
     fig = px.bar(count_df, x='neighbourhood', y='count', color=filter_type.lower(),
                 color_discrete_sequence=px.colors.qualitative.Set3,
                 category_orders={'neighbourhood': df['neighbourhood'].unique()})
 
-    # Customize the chart layout
+    # chart layout
     fig.update_layout(
         title=f"Count of {filter_type}s by Neighborhood",
         xaxis_title="Neighborhood",
@@ -115,7 +111,6 @@ def amenity_plot():
         xaxis={'categoryorder': 'array', 'categoryarray': df['neighbourhood'].unique()}
     )
 
-    # Display the chart in Streamlit
     st.plotly_chart(fig, use_container_width=True)
 
 
@@ -130,7 +125,7 @@ def amenity_distances_map():
             for neighbourhood in df['Arrondissement'].unique():
                 average_distance = df[(df['category'] == category) & (df['Arrondissement'] == neighbourhood)]['distance_in_m'].mean()
                 average_distances.append([category, neighbourhood, average_distance])
-    # Create a DataFrame from the list
+
         result_df = pd.DataFrame(average_distances, columns=['Category', 'Neighbourhood', 'Average Distance'])
         return result_df
 
@@ -138,31 +133,25 @@ def amenity_distances_map():
 
     cloropleth_df = pd.merge(ave_distance_df, polygons, left_on='Neighbourhood', right_on='Arrondissement', how='left')
 
-    # Drop the redundant 'Arrondissement' column
+    # redundant 'Arrondissement' column
     cloropleth_df.drop('Arrondissement', axis=1, inplace=True)
 
     # set nan values in the 'Average Distance' column to 0
     cloropleth_df['Average Distance'] = cloropleth_df['Average Distance'].fillna(0)
-
-    # drop the rest of the nan values
     cloropleth_df = cloropleth_df.dropna()
 
-    # Convert the DataFrame to a GeoDataFrame
+    # convert to geodataframe
     cloropleth_gdf = gpd.GeoDataFrame(cloropleth_df, geometry='geometry')
 
-    # Get unique category types
     categories = cloropleth_gdf['Category'].unique()
 
-    # Define initial category for the plot
     initial_category = categories[0]
-
-    # Filter DataFrame for initial category
     initial_category_df = cloropleth_gdf[cloropleth_gdf['Category'] == initial_category]
 
-    # Create choropleth map figure
+    # plotting
     fig = go.Figure()
 
-    # Add initial choropleth map trace
+    # initial choropleth map trace
     fig.add_trace(go.Choroplethmapbox(
         geojson=initial_category_df.geometry.__geo_interface__,
         locations=initial_category_df.index,
@@ -174,10 +163,9 @@ def amenity_distances_map():
         text=initial_category_df['Neighbourhood'],
         hoverinfo='text+z',
         zmin=0,
-        zmax=5500  # Set the maximum value for the legend
+        zmax=5500 # legend values
     ))
-
-    # Update layout
+t
     fig.update_layout(
         title=f'Average Distance to Amenity in Montreal',
         mapbox=dict(
@@ -188,7 +176,7 @@ def amenity_distances_map():
         margin=dict(l=0, r=0, t=0, b=0),
     )
 
-    # Define dropdown menu
+    # dropdown menu
     dropdown_menu = []
     for category in categories:
         category_df = cloropleth_gdf[cloropleth_gdf['Category'] == category]
@@ -203,7 +191,7 @@ def amenity_distances_map():
             )
         )
 
-    # Add dropdown menu to the figure
+    # dropdown menu to the figure
     fig.update_layout(updatemenus=[dict(
         buttons=dropdown_menu,
         direction="down",
@@ -215,14 +203,13 @@ def amenity_distances_map():
         yanchor="top"
     )])
 
-    # Display the plot
     st.plotly_chart(fig, use_container_width=True)
 
 
 # used in neighbourhood analysis
 def plot_neighborhood_graph(transportation_type, neighbourhood, distances_by_transportation, graphs_dict, amenity):
     
-    # Load the appropriate graph based on the transportation type
+    # appropriate graph based on the transportation type
     if transportation_type == "walking":
         G = graphs_dict["walking"][f"{neighbourhood}, Montreal, Canada"]
     elif transportation_type == "driving":
@@ -235,22 +222,18 @@ def plot_neighborhood_graph(transportation_type, neighbourhood, distances_by_tra
     
     distances = distances_by_transportation[distances_by_transportation["amenity"] == amenity]
     distances = distances[distances["neighborhood"] == f"{neighbourhood}, Montreal, Canada"]
-    # distances = distances_by_transportation[amenity][f"{neighbourhood}, Montreal, Canada"]
     
-    # Plot the graph with a light background
+    # plot graph
     fig, ax = ox.plot_graph(G_proj, figsize=(10, 8), bgcolor='white', edge_color='#CCCCCC', edge_linewidth=0.5, node_size=0, show=False, close=False)
     
     nodes_proj = ox.graph_to_gdfs(G_proj, edges=False)
     
-    # Scatter plot on the same Axes instance
+    # scatter
     sc = ax.scatter(x=nodes_proj['x'], y=nodes_proj['y'], c=distances['travel_time'], s=30, cmap='inferno_r', alpha=0.8, vmin=0, vmax=15)
     
-    # Create a colorbar
+    # create colourbar
     cbar = plt.colorbar(sc, ax=ax, shrink=0.7)
-    
-    # Set the colorbar ticks and tick labels
     cbar.set_ticks(range(0, 16))
     cbar.set_ticklabels(range(0, 16))
     
-    # Show the plot
     st.pyplot(fig)
